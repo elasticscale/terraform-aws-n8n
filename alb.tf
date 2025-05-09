f8f7f61d37dceee086d2afed280246600a4fe59f
Efi Weiss <efiwiss@gmail.com>
2025-05-09 22:09:04 +0300

    Fix typo in ssl policy

--- alb.tf
+++ alb.tf
@@ -61,7 +61,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-0"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.certificate_arn
   default_action {
     type             = "forward"
